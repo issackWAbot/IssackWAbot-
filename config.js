0cfab2dfcee21b817eab4d5e4f6d429017b93702
issackWAbot <issackpoonte@gmail.com>
2026-09-08 10:21:33 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ownerName: "Issack",
+  botName: "IssackWA-Bot",
+  ownerNumber: "919863955589",
+  gpayNumber: "919863955589",
+  prefix: ".",
+  coin: { daily: 50, cost: 5 }
+}
